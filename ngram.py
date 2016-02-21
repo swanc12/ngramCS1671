f7469c521d4cfc04f359f9d9a72dca2442e45fa1
Colin Swan <colin.swan@pitt.edu>
2016-02-20 23:59:43 -0500

    Made a bunch of bug fixes; began adding code to adjust weights

--- ngram.py
+++ ngram.py
@@ -1,7 +1,20 @@
+"""
+Author: Colin Swan
+
+Description: Implementation of an ngram model. Takes the type of ngram to use,
+the path to a training file, the path to a development file, and the path to
+the test file as inputs from the command line. Attempts to use the development
+file to determine good weights to use to achieve a lower entropy value.
+"""
+
 import re
 import math
 import collections
+import argparse
+
 #Courtesy of http://stackoverflow.com/questions/6027558/flatten-nested-python-dictionaries-compressing-keys
+#Used to flatten ngram dictionaries once probabilities have been determined,
+#which makes the results a bit easier to work with.
 def flatten(d, parent_key='', sep=' '):
     items = []
     for k, v in d.items():
@@ -13,21 +26,16 @@ def flatten(d, parent_key='', sep=' '):
     return dict(items)
 
 
-#General purpose ngram method
+#Gets the number of times each word / word pairs / etc. occur
 def ngram_freqs(in_str, n):
-  in_str = in_str.split(' ')
-  #Using a dict allows us to get the frequency of word combinations
-  #and reference them based on that combination of words.
-
+  in_str = in_str.strip().split(' ')
   output = {}
 
-  #Using range so we can group the inputs together
   include = 0
   if(n == 1):
     include = 1
+
   for i in range(include,len(in_str)-n+1):
-    #in_str[i:i+n] gets us the words from i to i+n, meaning
-    #if we want a bigram we'll get two words
     g = ' '.join(in_str[i:i+n])
     output.setdefault(g, 0)
     output[g] += 1
@@ -46,6 +54,8 @@ def ngram(in_str, n):
 
   freqs = ngram_freqs(in_str, n)
   probs = {}
+  #Creating a 2D Dict
+  #Form: {"wi-2 wi-1": {wi: 2}}
   for key in freqs:
     key_split = key.split(' ')
     base_word = key_split[len(key_split)-1:][0]
@@ -54,11 +64,17 @@ def ngram(in_str, n):
     probs.setdefault(given, {})
     probs[given][base_word] = freqs[key]
 
+  #Determing probabilities for words
+  #given previous words.
   for given in probs:
     total = 0
     given_dict = probs[given]
+    #Counting up total occurances
+    #of words after "given"
     for base in given_dict:
       total += given_dict[base]
+    #Dividing freq of each word by
+    #total occurances of the previous word(s)
     for base in given_dict:
       given_dict[base] = given_dict[base]/total
 
@@ -67,11 +83,13 @@ def ngram(in_str, n):
 
 def interpolate(in_str, ngram_list, weights):
 
+  #Keeping track of which level of ngram we're at
   curr_set = 0
   prob = 0
   for ngram in ngram_list:
     in_split = in_str.split(' ')
     curr_string = in_split[len(in_split)-1-curr_set:]
+
     if(curr_set == 0):
       curr_string = curr_string[0]
     else:
@@ -80,8 +98,33 @@ def interpolate(in_str, ngram_list, weights):
       prob += ngram[curr_string]*weights[curr_set]
     except KeyError:
       pass
+
     curr_set += 1
-  return -1*math.log(prob, 2)
+  if(prob == 0):
+    return 0
+  return math.log(prob, 2)
+
+def word_perplexity(in_str, ngram_list, n, smooth, weights):
+  in_str = in_str.split(' ')
+
+  entropy = 0
+  prob = 0
+  #-1 to remove count for <s>
+  word_count = len(in_str) - 1
+  for i in range(len(in_str)-n+1):
+    g = ' '.join(in_str[i:i+n])
+    if(smooth):
+      prob = interpolate(g, ngram_list, weights)
+    else:
+      try:
+        prob = math.log(ngram_list[n-1][g], 2)
+      except KeyError:
+        prob = 0
+        pass
+    entropy += prob
+
+  entropy = entropy * (1/word_count)
+  return entropy
 
 def replace_unknowns(in_str, k):
   #Getting frequency of words using unigram.
@@ -93,36 +136,98 @@ def replace_unknowns(in_str, k):
 
   return output
 
-def word_perplexity(in_str, ngram_list, n, weights):
-  in_str = in_str.split(' ')
+def replace_unk_ngram(unigram, in_str):
+  output = in_str
+  input_list = in_str.split(' ')
+  for word in input_list:
+    try:
+      unigram[word]
+    except KeyError:
+      output = re.sub(r"\s" + word + r"\s", " <unk> ", output)
 
-  entropy = 0
-  #-1 to remove count for <s>
-  word_count = len(in_str) - 1
-  for i in range(len(in_str)-n+1):
-    g = ' '.join(in_str[i:i+n])
-    prob = interpolate(g, ngram_list, weights)
-    entropy += prob
+  return output
 
-  entropy = entropy * (1/word_count)
-  return 2**entropy
 
-parsed = replace_unknowns("<s> a a , b h </s> <s> a b a i </s> <s> a b , a b a </s> <s> b b , j , b , a </s> <s> a a , a b , b </s> ", 1)
+##Main##
+parser = argparse.ArgumentParser()
+parser.add_argument(
+  'ngram',
+  type=str,
+  help='Ngram model to use. 1 = unigram, 2 = bigram, 2s = bigram smoothed,'+
+    ' 3 = trigram, 3s = trigram smoothed'
+)
+parser.add_argument('trainfile', type=str, help='Path to training file')
+parser.add_argument('devfile', type=str, help='Path to development file')
+parser.add_argument('testfile', type=str, help='Path to test file')
+args = parser.parse_args()
 
-#parsed = "<s> a b a b <unk> </s>"
+n = int(args.ngram[0])
+smoothed = False
+try:
+  if(args.ngram[1] == 's'):
+    smoothed = True
+except Exception:
+  pass
 
-result = ngram(parsed, 3)
-print(result)
-for key in result:
-  print(key + ': {0}'.format(result[key]))
+parsed = ''
+with open(args.trainfile, 'r') as trainfile:
+  for line in trainfile:
+    temp_parsed = line.strip().replace('.', ' </s>')
+    temp_parsed = temp_parsed.replace(',', ' ,')
+    temp_parsed = " <s> " + temp_parsed
+    temp_parsed = temp_parsed
+    parsed = parsed + temp_parsed
 
-print('~~~~~~~~~Unigram~~~~~~~~~~~~')
-print(ngram(parsed, 1))
+parsed = replace_unknowns(parsed, 1)
 
 all_ngrams = []
-for i in range(1, 4):
+for i in range(1, n+1):
   all_ngrams.append(ngram(parsed, i))
 
 
+lines = []
+with open(args.devfile, 'r') as devfile:
+  for line in devfile:
+    lines.append(line)
+
 print("~~~~~~~~~~~~perplexity test~~~~~~~~~~~~~")
-print(word_perplexity("<s> a a </s>", all_ngrams, 3, [.3, .3, .4]))
+test_text = ""
+weights = []
+if(n == 2):
+  weights = [.8, .2]
+elif(n == 3):
+  weights = [0, 0, 1]
+
+for line in lines:
+  test_text = replace_unk_ngram(all_ngrams[0], line.strip())
+  prev_perp = word_perplexity(test_text, all_ngrams, n, smoothed, weights)
+  print(test_text)
+  print(prev_perp)
+  weight1 = .5
+  weight2 = .5
+  weight1up = True
+  for i in range(100):
+    weights = [weight1, weight2]
+    new_perp = word_perplexity(test_text, all_ngrams, n, smoothed, weights)
+    if new_perp < prev_perp and not weight1up:
+      weight1 -= 0.05
+      weight2 += 0.05
+    elif new_perp > prev_perp and not weight1up:
+      weight1 += 0.05
+      weight2 -= 0.05
+      weight1up = True
+    elif new_perp < prev_perp and weight1up:
+      weight1 += 0.05
+      weight2 -= 0.05
+    else:
+      weight1 -= 0.05
+      weight2 += 0.05
+      weight1up = False
+    print("Weight1: {0} weight2: {1} perp: {2}".format(
+      weight1,
+      weight2,
+      new_perp
+    ))
+
+
+
